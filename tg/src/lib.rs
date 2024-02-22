6ddb0e74ae63fb993be92a4cea1be306d93e3e4d
bitful-pannul <bitful-pancreas@protonmail.com>
2024-02-22 15:42:10 -0300

    reply to first stack of messages

--- tg/src/lib.rs
+++ tg/src/lib.rs
@@ -2,7 +2,10 @@ use frankenstein::TelegramApi;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-use kinode_process_lib::{await_message, call_init, println, Address, Message};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+use kinode_process_lib::{await_message, call_init, println, timer, Address, Message};
 
 wit_bindgen::generate!({
     path: "wit",
@@ -43,20 +46,43 @@ fn handle_message(our: &Address, api: &mut Option<Api>) -> anyhow::Result<()> {
             }
             TgRequest::Hello => {
                 let updates_params = frankenstein::GetUpdatesParams {
-                    offset: None,
-                    limit: Some(1),
-                    timeout: None,
+                    offset: Some(21),
+                    limit: Some(10),
+                    timeout: Some(30),
                     allowed_updates: None,
                 };
 
                 let members_params = frankenstein::GetChatMemberCountParams {
                     chat_id: frankenstein::ChatId::Integer(6856598744),
                 };
-                let res = api
-                    .as_mut()
-                    .unwrap()
-                    .get_chat_member_count(&members_params)?;
+                let res = api.as_mut().unwrap().get_updates(&updates_params)?;
                 println!("got resss: {:?}", res);
+
+                let reply_params = frankenstein::ReplyParameters {
+                    chat_id: Some(frankenstein::ChatId::Integer(6856598744)),
+                    message_id: res.result[0].update_id as i32,
+                    allow_sending_without_reply: Some(true),
+                    quote: None,
+                    quote_entities: None,
+                    quote_parse_mode: None,
+                    quote_position: None,
+                };
+
+                let send_params = frankenstein::SendMessageParams {
+                    chat_id: frankenstein::ChatId::Integer(6856598744),
+                    text: "yes I see you!".to_string(),
+                    disable_notification: None,
+                    entities: None,
+                    message_thread_id: None,
+                    link_preview_options: None,
+                    parse_mode: None,
+                    protect_content: None,
+                    reply_markup: None,
+                    reply_parameters: Some(reply_params),
+                };
+
+                let res = api.as_mut().unwrap().send_message(&send_params);
+                println!("got response when sending: {:?}", res);
             }
         },
     }
