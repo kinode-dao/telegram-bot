e4595388576dee4db77e6d85e6113f8fe34f31a4
bitful-pannul <bitful-pancreas@protonmail.com>
2024-02-23 12:58:12 -0300

    forward updates to parent

--- tg/src/lib.rs
+++ tg/src/lib.rs
@@ -1,9 +1,10 @@
-use frankenstein::{MethodResponse, TelegramApi, Update};
+use frankenstein::{GetUpdatesParams, MethodResponse, Update};
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 
 use kinode_process_lib::{
-    await_message, call_init, get_blob, http::{HttpClientError, HttpClientResponse}, println, timer, Address, Message,
+    await_message, call_init, get_blob,
+    http::{HttpClientError, HttpClientResponse},
+    println, Address, Message, Request,
 };
 
 wit_bindgen::generate!({
@@ -19,43 +20,58 @@ use api::Api;
 
 #[derive(Debug, Serialize, Deserialize)]
 // #[serde_untagged]
-enum TgRequest {
-    Initialize { token: String },
-    Hello,
+struct TgInitialize {
+    token: String,
+    params: Option<GetUpdatesParams>,
 }
 
-// start process -> maintain loop -> increment offset ->
-//   get_updates() -> forward -> offset increment -> get_updates() -> get_updates
-//   expires?
-//   get_chat_members() -> http_call() ->
-fn handle_message(our: &Address, api: &mut Option<Api>) -> anyhow::Result<()> {
+#[derive(Debug, Serialize, Deserialize)]
+struct TgUpdate {
+    updates: Vec<Update>,
+}
+
+fn handle_message(
+    our: &Address,
+    api: &mut Option<Api>,
+    parent: &mut Option<Address>,
+) -> anyhow::Result<()> {
     let message = await_message()?;
 
     match message {
-        Message::Response { body, context, .. } => {
-            let response = serde_json::from_slice::<Result<HttpClientResponse, HttpClientError>>(&body)??;
+        Message::Response { body, .. } => {
+            let response =
+                serde_json::from_slice::<Result<HttpClientResponse, HttpClientError>>(&body)??;
 
-            if let HttpClientResponse::Http(response) = response {
-                println!("got respose with status: {:?}", response.status);
-                // give this back to the dawg that requested this? the parent?
-                let blob = get_blob();
-                if let Some(blob) = blob {
+            if let HttpClientResponse::Http(_) = response {
+                if let Some(blob) = get_blob() {
+                    // we could also just forward directly? maybe not
                     let response =
                         serde_json::from_slice::<MethodResponse<Vec<Update>>>(&blob.bytes)?;
-                    println!("got response !: {:?}", response);
 
-                    // TODO: forward the response to the parent
-
+                    // forward to parent
+                    if let Some(parent) = parent {
+                        let request = TgUpdate {
+                            updates: response.result.clone(),
+                        };
+                        let _ = Request::new()
+                            .target(parent.clone())
+                            .body(serde_json::to_vec(&request)?)
+                            .send();
+                    }
 
                     if let Some(api) = api {
                         // set api.current_offset based on the response, keep same if no updates
-                        let next_offset = response.result.last().map(|u| u.update_id + 1).unwrap_or(api.current_offset);
+                        let next_offset = response
+                            .result
+                            .last()
+                            .map(|u| u.update_id + 1)
+                            .unwrap_or(api.current_offset);
                         api.current_offset = next_offset;
 
                         let updates_params = frankenstein::GetUpdatesParams {
                             offset: Some(api.current_offset as i64),
                             limit: None,
-                            timeout: Some(10),
+                            timeout: Some(15),
                             allowed_updates: None,
                         };
 
@@ -66,30 +82,30 @@ fn handle_message(our: &Address, api: &mut Option<Api>) -> anyhow::Result<()> {
                 return Err(anyhow::anyhow!("unexpected Response: "));
             }
         }
-        Message::Request { ref body, .. } => match serde_json::from_slice(body)? {
-            TgRequest::Initialize { token } => {
-                let mut new_api = Api::new(&token, our.clone());
+        Message::Request {
+            ref body, source, ..
+        } => match serde_json::from_slice(body)? {
+            TgInitialize { token, params } => {
+                if source.node != our.node {
+                    return Err(anyhow::anyhow!(
+                        "got initialize request from foreign source {:?}",
+                        source
+                    ));
+                }
 
-                let updates_params = frankenstein::GetUpdatesParams {
+                let new_api = Api::new(&token, our.clone());
+
+                let updates_params = params.unwrap_or(GetUpdatesParams {
                     offset: Some(new_api.current_offset as i64),
                     limit: None,
-                    timeout: Some(10),
+                    timeout: Some(15),
                     allowed_updates: None,
-                };
+                });
 
                 new_api.request_no_wait("getUpdates", Some(updates_params))?;
 
+                *parent = Some(source);
                 *api = Some(new_api);
-            }
-            TgRequest::Hello => {
-                let members_params = frankenstein::GetChatMemberCountParams {
-                    chat_id: frankenstein::ChatId::Integer(6856598744),
-                };
-                let res = api
-                    .as_mut()
-                    .unwrap()
-                    .get_chat_member_count(&members_params)?;
-                println!("got response when sending: {:?}", res);
             }
         },
     }
@@ -103,8 +119,10 @@ fn init(our: Address) {
 
     // boot uninitialized, wait for initialize command.
     let mut api: Option<Api> = None;
+    let mut parent: Option<Address> = None;
+
     loop {
-        match handle_message(&our, &mut api) {
+        match handle_message(&our, &mut api, &mut parent) {
             Ok(()) => {}
             Err(e) => {
                 println!("tg: error: {:?}", e);
